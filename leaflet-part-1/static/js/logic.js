dd54e9283568896e27bc5e7572a01eb876d2c363
cnlamanna <cnlamanna@gmail.com>
2023-08-21 23:26:46 -0500

    completed part-1

--- leaflet-part-1/static/js/logic.js
+++ leaflet-part-1/static/js/logic.js
@@ -1,47 +1,69 @@
-// Fetch earthquake data
-fetch("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson")
-  .then(response => response.json())
-  .then(data => {
-    const map = L.map('map').setView([0, 0], 2);
+// Using d3.json to fetch earthquake data
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(data => {
+  let myMap = L.map("map", {
+    center: [39.8283, -98.5795],
+    zoom: 5
+  });
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
+
       attribution: 'Map data © <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    }).addTo(myMap);
+
+    const maxDepth = 700; // Maximum recorded depth (adjust as needed)
 
     function getColor(depth) {
-      // Implement a logic to return a color based on the depth value
-      // Example: You can use a gradient from light to dark based on depth
-      // Return a CSS color code like '#RRGGBB'
+      const colorScale = d3.scaleSequential(d3.interpolateRdYlGn).domain([maxDepth, 0]);
+      
+      return colorScale(depth);
     }
 
     function getRadius(magnitude) {
-      // Implement a logic to calculate the marker size based on magnitude
+      const scalingFactor = 5;
+      return Math.max(4, magnitude * scalingFactor);
     }
 
     L.geoJSON(data.features, {
       pointToLayer: function(feature, latlng) {
-        return L.circleMarker(latlng, {
-          radius: getRadius(feature.properties.mag),
-          fillColor: getColor(feature.geometry.coordinates[2]),
+        const coordinates = feature.geometry.coordinates;
+        const longitude = parseFloat(coordinates[0]);
+        const latitude = parseFloat(coordinates[1]);
+        const depth = parseFloat(coordinates[2]);
+        const mag = feature.properties.mag || 0;
+        
+        return L.circleMarker(L.latLng(latitude, longitude), {
+          radius: getRadius(mag),
+          fillColor: getColor(depth),
+          // border colors
           color: "#000",
           weight: 1,
-          opacity: 1,
           fillOpacity: 0.8
+
+          //binding popup to each layer
         }).bindPopup(
-          "Magnitude: " + feature.properties.mag +
-          "<br>Depth: " + feature.geometry.coordinates[2] +
+          "Magnitude: " + mag +
+          "<br>Depth: " + depth +
           "<br>Location: " + feature.properties.place
         );
       }
-    }).addTo(map);
+    }).addTo(myMap);
 
-    // Add a legend
+    // setting up legend
     const legend = L.control({ position: 'bottomright' });
-    legend.onAdd = function (map) {
-      const div = L.DomUtil.create('div', 'info legend');
-      // Implement the legend content here
+    legend.onAdd = function (myMap) {
+    const div = L.DomUtil.create('div', 'info legend');
+    const numGrades = 5; // Number of gradient steps
+// continued.. legend 
+  div.innerHTML += '<h4>Depth Legend</h4>';
+  for (let i = 0; i < numGrades; i++) {
+    const depthValue = maxDepth / numGrades * (i + 1);
+    const color = getColor(depthValue);
+    div.innerHTML +=
+      '<i style="background:' + color + '"></i> ' +
+      depthValue.toFixed(2) + ' km<br>';
+  }
+
       return div;
     };
-    legend.addTo(map);
+    legend.addTo(myMap);
   });
